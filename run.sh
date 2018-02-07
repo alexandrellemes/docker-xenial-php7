66401c73311375b6a112dc8d5a1f55c8cd184603
Alexandre Pereira LLemes <alexandre.llemes@gmail.com>
2018-02-07 01:34:41 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t "xenial/php7:lastest" .
+docker build -t "xenial/php7:latest" .
 
 docker run -it -p 80:80 --network=host --privileged -v $HOME/discoDocker/apache/projetos/:/var/www/html/ -v $HOME/discoDocker/apache/conf/:/etc/apache2/sites-available -v /tmp:/tmp --name apache -d xenial/php7:latest
